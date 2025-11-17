b95b8d8e5b0297224c0c9e9745b4902ef1862c6b
Jessica McKinnie <jess@deathweasel.io>
2025-11-17 08:08:41 -0500

    more dns entries

--- dns/main.tf
+++ dns/main.tf
@@ -10,3 +10,37 @@ resource "cloudflare_zone" "kd3bwz_net" {
     name = "kd3bwz.net"
     type = "full"
 }
+
+resource "cloudflare_dns_record" "kd3bwz_net_root" {
+    name = "kd3bwz.net"
+    type = "A"
+    content = "152.42.153.153"
+    zone_id = cloudflare_zone.kd3bwz_net.id
+    ttl = 1
+    proxied = true
+}
+
+resource "cloudflare_dns_record" "kd3bwz_dmarc" {
+    name = "_dmarc.kd3bwz.net"
+    type = "TXT"
+    content = "\"v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s; rua=mailto:admin@deathweasel.io\""
+    ttl = 1
+    zone_id = cloudflare_zone.kd3bwz_net.id
+}
+
+resource "cloudflare_dns_record" "kd3bwz_domainkey" {
+    name = "*._domainkey.kd3bwz.net"
+    type = "TXT"
+    content = "\"v=DKIM1; p=\""
+    ttl = 1
+    zone_id = cloudflare_zone.kd3bwz_net.id
+}
+
+resource "cloudflare_dns_record" "kd3bwz_net_spf" {
+    name = "kd3bwz.net"
+    type = "TXT"
+    content = "\"v=spf1 -all\""
+    ttl = 1 
+    zone_id = cloudflare_zone.kd3bwz_net.id
+
+}
--- dns/secrets.tf
+++ dns/secrets.tf
@@ -5,5 +5,4 @@ data "sops_file" "secrets" {
 locals {
     cloudflare_token = data.sops_file.secrets.data["cloudflare_api_token"]
     cloudflare_account_id = data.sops_file.secrets.data["cloudflare_account_id"]
-    cloudflare_zone_id = data.sops_file.secrets.data["cloudflare_zone_id"]
 }
